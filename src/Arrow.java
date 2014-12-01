ab8c46a8c5106c5a8c04ba35c6052ca87dd9975b
chiquelo <eduardo.coronado@hotmail.com>
2014-11-30 23:39:57 -0500

    Comments for Javadoc

--- src/Arrow.java
+++ src/Arrow.java
@@ -4,86 +4,137 @@
  * Arrow is usually defined by the row and column of the block where it is, and it's orientation (north, west, south. east)
  */
 
+/**
+ * 
+ * @author Serge Nasr
+ *
+ */
 public class Arrow {
 	private int row, column;
 	private char point;	//checks where arrow is poiting (north, east, south or west)
+	/**
+	 * An Arrow contains an arrow that is used as a cursor throughout localisation
+	 */
 	private Arrow next;
 	private double x, y, theta;
-
+	/**
+	 * Arrow is usually defined by the row and column of the block where it is, and it's orientation (north, west, south. east)
+	 * @param r
+	 * @param c
+	 * @param p
+	 */
 	public Arrow(int r, int c, char p) {
 		row = r;
 		column = c;
 		point = p;
-		next = this;	//next points to the latest arrow for the robot to check
-		/*
-		 * When a robot turns or moves forward, our algorithm only check if the next arrow can be correctly updated
-		 * Based on this result, our algorithm either removes the current arrow from the possible solutions or updates the "next" arrow
-		 */
+		next = this;
 	}
-	
+	/**
+	 * Arrow can be defined by its x, y coordinates and orientation
+	 * @param x
+	 * @param y
+	 * @param theta
+	 */
 	public Arrow(double x, double y, double theta) {
 		this.x = x;
 		this.y = y;
 		this.theta = theta;
 	}
-
+	/**
+	 * Set the row of the arrow
+	 * @param row
+	 */
 	public void setRow(int row) {
 		this.row = row;
 	}
-
+	/**
+	 * Set the column of the arrow
+	 * @param column
+	 */
 	public void setColumn(int column) {
 		this.column = column;
 	}
-
+	/**
+	 * Set the direction of the arrow
+	 * @param point
+	 */
 	public void setPoint(char point) {
 		this.point = point;
 	}
-
+	/**
+	 * Get the x coordinate
+	 * @return x value on a graph
+	 */
 	public double getX() {
 		return x;
 	}
-
+	
+	/**
+	 * Get the y coordinate
+	 * @return y value on a graph
+	 */
 	public double getY() {
 		return y;
 	}
-
+	
+	/**
+	 * get the direction
+	 * @return Value can either be 0,90,180 or 270
+	 */
 	public double getTheta() {
 		return theta;
 	}
-
+	/**
+	 * Get the cursor of the arrow
+	 * @return an Arrow object
+	 */
 	public Arrow getNext() {
 		return next;
 	}
-
-	public void setNext(Arrow next) {
-		this.next = next;
-	}
-
+	/**
+	 * Get the row
+	 * @return row number from 0 to 11
+	 */
 	public int getRow() {
 		return row;
 	}
-
+	/**
+	 * Get the column
+	 * @return column number from 0 to 11
+	 */
 	public int getColumn() {
 		return column;
 	}
-
+	/**
+	 * Get the direction
+	 * @return a char that is either 'n', 's', 'w' or 'e'
+	 */
 	public char getPoint() {
 		return point;
 	}
-
-	//the following methods convert from row and column of block to X and Y coordinates
-	//and the heading of the arrow to the theta angle
-	
+	/**
+	 * Converts a column to an x coordinate
+	 * @param col
+	 * @return the x coordinate of the arrow
+	 */
 	public static double columnToY (int col) {
 		return (- 15 + 30 * col); 
 	}
-
+	/**
+	 * Converts a row to a y coordinate
+	 * @param row
+	 * @return the y coordinate of the arrow
+	 */
 	public static double rowToX (int row) {
 		return (-15 +30*row); 
 	}
-
-	public static double headToTheta (char head) {
-		switch (head) {
+	/**
+	 * Converts from char convention to angle
+	 * @param direction
+	 * @return the angle represented by the char direction
+	 */
+	public static double headToTheta (char direction) {
+		switch (direction) {
 		case 'n':
 			return 0;
 		case 'w':
@@ -96,37 +147,11 @@ public class Arrow {
 			return -11111;	//error
 		}
 	}
-	
-	public static int xToRow (int x) {
-		switch (x) {
-		case -15:
-			return 0;
-		case 15:
-			return 1;
-		case 45:
-			return 2;
-		case 75:
-			return 3;
-		default:
-				return 1111; //error
-		}
-	}
-	
-	public static int yToCol (int y) {
-		switch (y) {
-		case -15:
-			return 0;
-		case 15:
-			return 1;
-		case 45:
-			return 2;
-		case 75:
-			return 3;
-		default:
-				return 1111; //error
-		}
-	}
-	
+	/**
+	 * Converts from and angle to a direction
+	 * @param theta
+	 * @return the char representing the given angle ('n','s','w' or 'e')
+	 */
 	public static char thetaToHead (double theta) {
 		if (theta == 0) {
 			return 'n';
